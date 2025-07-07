4edf94b632351f85ee093be30711453d821eef91
shahwaleed1 <its.waleed33@gmail.com>
2025-07-07 16:46:07 +0500

    login

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -8,9 +8,24 @@ export const register = async(req, res) => {
         const newAdmin = new adminModel({ name, email, password });
         await newAdmin.save()
 
-
+        return res.status(200).json({ message: 'successfull registion!'})
     }
 
+    catch(err){
+        return res.status(500).json({ message: err.message})
+    }
+}
+
+
+
+
+export const login = async(req, res) => {
+
+    const { email, password } = req.body;
+
+    try{
+        const admin = await adminModel.findOne({ email })
+    }
     catch(err){
         return res.status(500).json({ message: err.message})
     }
