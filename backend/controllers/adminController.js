addb574bd8d65214f339cda7a39edb3e0878ff25
shahwaleed1 <its.waleed33@gmail.com>
2025-07-08 22:35:21 +0500

    admin login

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,5 @@
 import adminModel from "../models/adminModel";
+import bcrypt, { hash } from 'bcryptjs'
 
 export const register = async(req, res) => {
     const { name, email, password } = req.body;
@@ -24,7 +25,17 @@ export const login = async(req, res) => {
     const { email, password } = req.body;
 
     try{
-        const admin = await adminModel.findOne({ email })
+        const admin = await adminModel.findOne({ email });
+        if(!admin){
+            return res.status(404).json({ message: 'Not Found' });
+        }
+
+        const isMatch = await bcrypt.compare(password, admin.password);
+        if(!isMatch){
+            return res.status(401).json({ message: 'Incurrect Password' });
+        }
+
+        return res.status(200).json({ message: 'Login successful!'})
     }
     catch(err){
         return res.status(500).json({ message: err.message})
