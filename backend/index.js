777ff7a6aa39c85d5248c70072a1c0f863530f94
shahwaleed1 <its.waleed33@gmail.com>
2025-09-10 23:14:19 +0500

    added testing api endpoint

--- backend/index.js
+++ backend/index.js
@@ -40,18 +40,18 @@ const upload = multer({ storage })
 
 
 
-app.get('/',(req, res)=>{
+app.get('/hi', (req, res) => {
     return res.status(200).json('Hello word! 2025')
 })
 
 
 
 
-app.post('/api/publish',upload.single('icon'), async(req, res)=>{
+app.post('/api/publish', upload.single('icon'), async (req, res) => {
 
     const iconUrl = `http://localhost:5000/uploads/${req.file.filename}`;
 
-    try{
+    try {
         const newApp = new AppModel({
             ...req.body,
             icon: iconUrl
@@ -60,27 +60,27 @@ app.post('/api/publish',upload.single('icon'), async(req, res)=>{
 
 
         res.status(200).json({
-            message : 'App added in DB!',
+            message: 'App added in DB!',
             data: savedApp,
         })
 
     }
-    catch(err){
-        console.log('Error in Publish api :', err )
-        res.status(500).json({err: 'network error occur publish '})
+    catch (err) {
+        console.log('Error in Publish api :', err)
+        res.status(500).json({ err: 'network error occur publish ' })
     }
 })
 
 
 
 
-app.get('/api/apps', async(req, res) =>{
-    try{
+app.get('/api/apps', async (req, res) => {
+    try {
         const apps = await AppModel.find();
         res.status(200).json(apps)
     }
-    catch(err){
-        console.log('Error in apps :', err )
+    catch (err) {
+        console.log('Error in apps :', err)
         res.status(500).json({ error: err.message });
     }
 })
@@ -88,33 +88,33 @@ app.get('/api/apps', async(req, res) =>{
 
 
 
-app.delete('/api/apps/:id', async(req, res)=>{
-    try{
+app.delete('/api/apps/:id', async (req, res) => {
+    try {
         const appdelete = await AppModel.findByIdAndDelete(req.params.id);
-        res.status(200).json({msg: 'App deleted successfully' , appdelete})
+        res.status(200).json({ msg: 'App deleted successfully', appdelete })
     }
-    catch(err){
+    catch (err) {
         console.error(err)
     }
 })
 
 
 app.put('/api/apps/:id', async (req, res) => {
-    try{
+    try {
         const updateApp = await AppModel.findByIdAndUpdate(
             req.params.id,
             req.body,
-            { new : true, runValidators: true}
+            { new: true, runValidators: true }
         )
         if (!updateApp) {
             return res.status(404).json({ message: 'App not found' });
         }
         res.status(200).json({ msg: 'App Updated Success', updateApp })
-    } 
-    catch(err){
+    }
+    catch (err) {
         console.log('Error updating app: ', err)
         res.status(500).json({ error: 'Server error' });
-    }   
+    }
 })
 
 
@@ -126,30 +126,30 @@ app.get('/api/apps/:id', async (req, res) => {
     //     return res.status(400).json({ message: 'Invalid ID format'})
     // }
 
-    try{
+    try {
         const app = await AppModel.findById(id)
-        if(!app) return res.status(404).json({ message: 'not found!'})
+        if (!app) return res.status(404).json({ message: 'not found!' })
         res.status(200).json(app)
     }
-    catch(err){
+    catch (err) {
         console.log(err)
-        res.status(500).json({ message: 'Server Error : ' ,err });
+        res.status(500).json({ message: 'Server Error : ', err });
     }
 })
 
-app.get('/api/admin/admins', async(req, res) => {
-    try{
+app.get('/api/admin/admins', async (req, res) => {
+    try {
         const admins = await adminModel.find()
         res.status(200).json(admins)
     }
-    catch(err){
+    catch (err) {
         console.log('Error in admims getting Data : ', err);
         res.status(500).json({ error: err.message })
     }
 })
 
 app.delete('/api/admin/admins/:id', async (req, res) => {
-    const  id = req.params.id;
+    const id = req.params.id;
     try {
         const admin = await adminModel.findByIdAndDelete(id);
         return res.status(204).json({ message: admin });
@@ -169,4 +169,4 @@ app.use('/api/admin', adminRoutes);
 
 
 
-app.listen(PORT, ()=> console.log(`Server start on ${PORT}`))
+app.listen(PORT, () => console.log(`Server start on ${PORT}`))
